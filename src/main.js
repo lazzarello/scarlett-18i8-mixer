63dc272f2fd5cb6d0e3c12316eac35231a950259
Lee Azzarello <lee.azzarello@astraspace.com>
2020-08-04 02:44:34 -0700

    I don't know what I'm doing but weee!

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
-import axios from 'vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   mounted () {
-    axios.get('http://localhost:1234/jsonapi?request=get-card-all').then(response => (this.info = response))}
+    axios.get('http://localhost:1234/jsonapi?request=card-get-all').then(response => (this.info = response))}
 }).$mount('#app')
