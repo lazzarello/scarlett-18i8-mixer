5544651708314664e63f5736dbf35c0dd3e67ce6
Lee Azzarello <lee.azzarello@astraspace.com>
2020-08-06 12:45:12 -0700

    now I know how to use components!

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Scarlett cardinfo="cardinfo should be a variable" />
-    <h1>The soundcards</h1>
-      <p>Device Name: {{ info.cardid }}</p>
-      <p>Device Info: {{ info.info }}</p>
+    <Scarlett />
   </div>
 </template>
 
-
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
   import Scarlett from './components/Scarlett.vue'
-  import axios from 'axios'
-  var apiURL = 'http://localhost:1234/jsonapi?request=';
 
   export default {
-    data () {
-      return {
-        info: null
-      }
-    },
-    mounted () {
-      axios.get(apiURL + 'card-get-all').then(response => (this.info = response.data.data[3]));
-    },
     name: 'App',
     components: {
-      HelloWorld,
       Scarlett
     }
   }
--- src/components/Scarlett.vue
+++ src/components/Scarlett.vue
@@ -1,14 +1,43 @@
 <template>
   <div class="scarlett">
-    <tt> {{ cardinfo }} </tt>
+    <h1>Scarlett 18i8 Mixer</h1>
+      <p>Found Device: <tt>{{ info | findScarlett }}</tt></p>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  var apiURL = 'http://localhost:1234/jsonapi?request=';
   export default {
-    name: 'Scarlett',
-    props: {
-      cardinfo: String
-    }
+    data () {
+      return {
+        info: null
+      }
+    },
+    filters: {
+      findScarlett ( cardArray ) {
+        var outInfo = {}
+        var found = false
+        // this is kind ugly but whatevs
+        let re = RegExp('.*18i8.*')
+        cardArray.forEach(val => {
+          if ( re.test(val.name) ) {
+            found = true
+            outInfo = val
+          }
+        })
+        if ( found ) {
+          return outInfo.info
+        } else {
+          outInfo = {"error": "No Scarlett device found"}
+          return outInfo
+        }
+      }
+    },
+    mounted () {
+      axios.get(apiURL + 'card-get-all').then(response => (this.info = response.data.data));
+      // need to do error handling
+    },
+    name: 'Scarlett'
   }
 </script>
