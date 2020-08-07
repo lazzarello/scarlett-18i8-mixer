724ba5f616d18bc3c2cc6d2802c63c83fd8b0b95
Lee Azzarello <lee.azzarello@astraspace.com>
2020-08-07 01:11:00 -0700

    eslint overrides for debugging

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue' 
+// eslint-disable-next-line no-unused-vars
+import Nexus from 'nexusui'
 
 Vue.config.productionTip = false;
-//Vue.config.runtimeCompiler = true;
+// Vue.config.runtimeCompiler = true;
+
+// https://nexus-js.github.io/ui/api/#intro
+// the following is a convention for defaults of Vue
+// eslint-disable-next-line no-unused-vars
+//var dial = new Nexus.Dial('#dial')
 
 new Vue({
   render: h => h(App),
