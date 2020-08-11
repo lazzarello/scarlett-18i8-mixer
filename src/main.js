950a0030a92b6e1a8b6ff8c42fc2cdf5e2ffcf75
Lee Azzarello <lee.azzarello@astraspace.com>
2020-08-11 16:39:47 -0700

    clean up imports/add component doc

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue' 
-// eslint-disable-next-line no-unused-vars
-import Nexus from 'nexusui'
 
 Vue.config.productionTip = false;
 // Vue.config.runtimeCompiler = true;
-
-// https://nexus-js.github.io/ui/api/#intro
-// the following is a convention for defaults of Vue
-// eslint-disable-next-line no-unused-vars
-//var dial = new Nexus.Dial('#dial')
 
 new Vue({
   render: h => h(App),
